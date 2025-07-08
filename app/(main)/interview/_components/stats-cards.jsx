a6b5c6e1a74af50133164628916a70a369e6bd8c
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-08 13:44:18 +0300

    perfomance chart and quiz list and the stats card

--- app/(main)/interview/_components/stats-cards.jsx
+++ app/(main)/interview/_components/stats-cards.jsx
@@ -32,7 +32,7 @@ const StatsCards = ({ assessments }) => {
   };
 
   return (
-    <div className="grid gap-4 md:grid-cols-3">
+    <div className="grid gap-4 md:grid-cols-3 pb-4">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Average Score</CardTitle>
