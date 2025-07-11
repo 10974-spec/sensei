9bab12aabb871ce16240cf8c98f6c41375a9caff
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-11 12:57:17 +0300

    helper function for the resume builder

--- app/(main)/resume/_components/resume-builder.jsx
+++ app/(main)/resume/_components/resume-builder.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Download, Save } from "lucide-react";
+import { AlertTriangle, Download, Edit, Monitor, Save } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
@@ -12,9 +12,12 @@ import { saveResume } from "@/actions/resume";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { EntryForm } from "./entry-form";
+import { entriesToMarkdown } from "@/app/lib/helper";
 
 const ResumeBuilder = ({ initialContent }) => {
   const [activeTab, setActiveTab] = useState("edit");
+  const [resumeMode, setResumeMode] = useState("preview");
+  const [previewContent, setPreviewContent] = useState(initialContent);
 
   const {
     control,
@@ -46,6 +49,43 @@ const ResumeBuilder = ({ initialContent }) => {
   useEffect(() => {
     if (initialContent) setActiveTab("preview");
   }, [initialContent]);
+
+  useEffect(()=>{
+    if (activeTab === "edit"){
+      const newContent = getCombinedContent();
+      setPreviewContent(newContent ? newContent : initialContent);
+    }
+
+  },[formValues,activeTab])
+
+   const getContactMarkdown = () => {
+    const { contactInfo } = formValues;
+    const parts = [];
+    if (contactInfo.email) parts.push(`📧 ${contactInfo.email}`);
+    if (contactInfo.mobile) parts.push(`📱 ${contactInfo.mobile}`);
+    if (contactInfo.linkedin)
+      parts.push(`💼 [LinkedIn](${contactInfo.linkedin})`);
+    if (contactInfo.twitter) parts.push(`🐦 [Twitter](${contactInfo.twitter})`);
+
+    return parts.length > 0
+      ? `## <div align="center">${user.fullName}</div>
+        \n\n<div align="center">\n\n${parts.join(" | ")}\n\n</div>`
+      : "";
+  };
+
+  const getCombinedContent = () => {
+    const { summary, skills, experience, education, projects } = formValues;
+    return [
+      getContactMarkdown(),
+      summary && `## Professional Summary\n\n${summary}`,
+      skills && `## Skills\n\n${skills}`,
+      entriesToMarkdown(experience, "Work Experience"),
+      entriesToMarkdown(education, "Education"),
+      entriesToMarkdown(projects, "Projects"),
+    ]
+      .filter(Boolean)
+      .join("\n\n");
+  };
 
   const onSubmit = async (data) => {};
 
@@ -243,7 +283,31 @@ const ResumeBuilder = ({ initialContent }) => {
 
           </form>
         </TabsContent>
-        <TabsContent value="preview">Change your password here.</TabsContent>
+        <TabsContent value="preview">
+          <Button variant="link" type="button" 
+          onClick={() => setResumeMode(resumeMode === "preview" ? "edit" : "preview")}
+          className="mb-2">
+           {resumeMode === "preview" ? (
+            <>
+             <Edit className="h-4 w-4"/>
+            Edit Resume
+            </>
+           ): (
+            <>
+              <Monitor className="h-4 w-4"/>
+              Show Preview
+            </>
+           )}
+          </Button>
+          {resumeMode !== "preview" && (
+            <div className="flex p-3 gap-2 items-center border-2 border-yellow-600 text-yellow-600 rounded mb-2"> 
+            <AlertTriangle className="h-5 w-5"/>
+            <span className="text-sm">
+              You will lose edited markdown if you update the form data.
+            </span>
+            </div>
+          )}
+        </TabsContent>
       </Tabs>
     </div>
   );
